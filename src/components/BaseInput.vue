4d9c995f7a7b5a7e5a84980b3bf489e824f38271
kheangmeng <kheangmengte@gmail.com>
2024-08-11 18:08:25 +0700

    test define model with form

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+type inputType = 'text' | 'number' | 'password' | 'textarea' | 'search' | 'url' | 'email' | 'tel'
+interface Props {
+  placeholder?: string
+  label?: string
+  disabled?: boolean
+  type: inputType
+}
+withDefaults(defineProps<Props>(), {
+  label: 'Input value',
+  placeholder: 'Enter Input value',
+  type: 'text'
+})
+const model = defineModel<string | number | null | undefined>()
+</script>
+<template>
+  <input
+    v-model="model"
+    :placeholder="placeholder"
+    :label="label"
+    :disabled="disabled"
+    :type="type"
+  />
+</template>
---
+++ src/components/LoginForm.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import BaseInput from '@/components/BaseInput.vue'
+
+const username = defineModel('username', { default: '' })
+const password = defineModel('password', { default: '' })
+</script>
+
+<template>
+  <BaseInput v-model="username" />
+  <BaseInput v-model="password" type="password" />
+</template>
